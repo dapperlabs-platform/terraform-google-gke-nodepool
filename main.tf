702370418e764136e14265464dd312642cd0d18f
Tommy <tommy.cho@dapperlabs.com>
2025-02-10 13:44:20 -0800

    dyanmic block

--- main.tf
+++ main.tf
@@ -76,11 +76,13 @@ resource "google_container_node_pool" "nodepool" {
     tags              = var.node_tags
     boot_disk_kms_key = var.node_boot_disk_kms_key
     resource_labels   = var.resource_labels
-    kubelet_config {
-      cpu_manager_policy = var.enable_kubelet_config == "none" ? "none" : "static"
-      cpu_cfs_quota = var.cpu_cfs_quota
-      pod_pids_limit = var.pod_pids_limit
-
+    dynamic "kubelet_config" {
+      for_each = var.enable_kubelet_config == "static" ? [1] : []
+      content {
+        cpu_manager_policy = var.enable_kubelet_config
+        cpu_cfs_quota     = var.cpu_cfs_quota
+        pod_pids_limit    = var.pod_pids_limit
+      }
     }
 
     dynamic "guest_accelerator" {
--- variables.tf
+++ variables.tf
@@ -220,11 +220,9 @@ variable "enable_kubelet_config" {
 variable "cpu_cfs_quota" {
   description = "Enable cpu cfs quota for the node pool."
   type        = bool
-  default     = false
 }
 
 variable "pod_pids_limit" {
   description = "Enable pod pids limit for the node pool."
   type        = number
-  default     = 0
 }
